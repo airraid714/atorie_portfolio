ed14758ce14cfef0ad395eba43e1465c293b34e6
kota machino <65933978+airraid714@users.noreply.github.com>
2023-07-14 13:46:37 +0900

    グローバルナビゲーションのhoverアニメーション設定

--- src/js/side-bar.js
+++ src/js/side-bar.js
@@ -1,8 +1,21 @@
 jQuery( function($){
+
+    // ハンバーガーボタンをクリックするとサイドバーが出現する
     $(".js-side-bar").on("click",function(){		
         $(".l-sidebar").toggleClass("is-open");
         $(".c-button--hamburger").toggleClass("is-open");
         $(".p-navigation--sidebar").toggleClass("is-open");
+    });
+
+    // メニューのリストをクリックするとサイドバーが閉じる
+    $(".js-close").on("click",function(){
+
+        setTimeout(function(){
+            $(".l-sidebar").removeClass("is-open");
+            $(".c-button--hamburger").removeClass("is-open");
+            $(".p-navigation--sidebar").removeClass("is-open");
+        },500);
+
     });
 
 
